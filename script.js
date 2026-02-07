d4571a05ef78ca356798a5cc2aaf1af4a8620409
Jey <jeyak1263@gmail.com>
2026-02-07 18:09:19 +0530

    41%

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-const form = document.getElementById("contactForm");
-const status = document.getElementById("status");
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
 
-  const data = {
-    name: name.value,
-    email: email.value,
-    message: message.value
-  };
-
-  const res = await fetch("http://localhost:5000/api/contact", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
-
-  const result = await res.json();
-  status.innerText = result.message;
-  form.reset();
-});
+hamburger.onclick = () => {
+  navMenu.classList.toggle("show");
+};
